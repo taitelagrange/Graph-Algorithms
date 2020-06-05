acb23c26033212ccf52535e9d1a816d634056532
Taite <lagr1780@mylaurier.ca>
2020-06-04 21:00:32 -0400

    Exchange working. Next step is building exchange graph.

--- 1A_GraphResearch/src/graph.py
+++ 1A_GraphResearch/src/graph.py
@@ -14,6 +14,7 @@ class Graph:
         """
         self._vertices = {}
         self._size = 0
+        self._exchanges = 0
         return
 
     def addVertex(self, key, edges):
@@ -40,7 +41,8 @@ class Graph:
     def addEdge(self, key1, key2):
         """
         -------------------------------------------------------
-        Adds an edge between two given vertices.
+        Adds an edge between two given vertices. Edge must not already exist.
+        Vertices must already exist.
         Use: graph.addEdge(key1, key2)
         -------------------------------------------------------
         Preconditions:
@@ -48,12 +50,13 @@ class Graph:
             key2 - second vertex (str)
         -------------------------------------------------------
         """
-        if key1 in self._vertices.keys() and key2 in self._vertices.keys():
+        if key1 in self._vertices.keys() and key2 in self._vertices.keys() and key2 not in self._vertices[key1]:
             self._vertices[key1].append(key2)
             self._vertices[key2].append(key1)
         else:
-            print("Invalid vertex.")
+            print("Invalid edge.")
         return
+    
     def getGraph(self):
         """
         -------------------------------------------------------
@@ -65,3 +68,130 @@ class Graph:
             print(i, ":", sorted(j))
         return
     
+    def checkDegrees(self):
+        """
+        -------------------------------------------------------
+        Asserts that all vertices in the graph are odd degree.
+        Use: valid = graph.checkDegrees()
+        -------------------------------------------------------
+        Postconditions:
+            True if all vertices of odd degree, False otherwise
+        -------------------------------------------------------
+        """
+        for i in self._vertices.keys():
+            if len(self._vertices[i]) % 2 == 0:
+                return False
+        return True
+    
+    def find_all_H_paths(self, start, end, path=[]):
+        """
+        Sourced from https://www.python.org/doc/essays/graphs/ with minor modifications.
+        ******For testing
+        """
+        path = path + [start]
+        if start == end:
+            return [path]
+        if start not in self._vertices.keys():
+            return []
+        paths = []
+        for node in self._vertices[start]:
+            if node not in path:
+                newpaths = self.find_all_H_paths( node, end, path)
+                for newpath in newpaths:
+                    if len(newpath) == self._size:
+                        paths.append(newpath)
+        return paths
+    
+    def _exchange(self, path, removed):
+        """
+        -------------------------------------------------------
+        Helper function. Performs an exchange between an edge connected to the
+        last vertex but not in the path and an edge in the path to create a new path.
+        Use: graph._exchange(path)
+        -------------------------------------------------------
+        Preconditions:
+            path - vertices in order of traversal from start to finish (list of str)
+            removed - edges already deleted through exchange (list of str)
+        -------------------------------------------------------
+        """
+        flag = 0
+        newpath = []
+        #get edge to add
+        i = 0
+        newend = self._vertices[path[-1]][i]
+        newedge = [path[-1], newend]
+        
+        while (newend is path[0] or newedge == removed or path[-2] == newend) and i < len(self._vertices[path[-1]])-1:
+            i+=1
+            newend = self._vertices[path[-1]][i]
+            newedge = [path[-1], newend]
+            
+        if newedge == removed:
+            #no exchange possible
+            return None, None
+        i = 0
+        
+        #add new edge and remove edge
+        while path[i] != newend:
+            newpath.append(path[i])
+            i+=1
+        newpath.append(newend)
+        for j in path[len(path):i:-1]:
+            newpath.append(j)
+                
+        #print(newpath)
+        #removed.append([newpath[-1], newend])
+        removed = [newpath[-1], newend]
+        self._exchanges += 1
+        if newpath[0] in self._vertices[newpath[-1]]:
+            flag = 1
+        return newpath, removed, flag
+        
+    def runThomason(self, path):
+        """
+        -------------------------------------------------------
+        Runs Thomason's Algorithm on a given graph. Every vertex must be of odd degree.
+        Use: graph.runThomason(edge, path)
+        -------------------------------------------------------
+        Preconditions:
+            edge - the edge that must be contained in every cycle, the starting edge
+                    of the path in form [v1, v2] (list of str)
+            path - vertices in order of traversal from start to finish (list of str)
+        Postconditions:
+        
+        -------------------------------------------------------
+        """
+        if not self.checkDegrees():
+            #not a valid graph for algorithm
+            return
+        
+        #exchangeG = Graph()
+        removed = []
+        newpath = path
+        flag = 0
+        while newpath is not None and not flag:
+            newpath, removed, flag = self._exchange(newpath, removed)
+            """"if newpath is not None:
+                exchangeG.addVertex(newpath, None)
+                exchangeG.addEdge(newpath, )"""
+            print(newpath, removed)
+     
+        #create exchange graph
+        #possibly make it only store last removed edge, in case there is new path with that edge
+        #would need to check H path is not already in graph, otherwise infinite loop
+        return
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- 1A_GraphResearch/src/testing.py
+++ 1A_GraphResearch/src/testing.py
@@ -1,12 +1,36 @@
 from graph import Graph
 
 g = Graph()
-nodes = ["A", "B", "C", "D", "E", "F"]
+nodes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
 for i in nodes:
     g.addVertex(i, None)
 
-for i in range(1, 5, 2):
+"""for i in range(1, 5, 2):
     g.addEdge(nodes[i-(i%2)],nodes[i])
-g.addEdge("A", "C")
+g.addEdge("E", "F")
+g.addEdge("C", "B")
+g.addEdge("B", "F")
+g.addEdge("C", "F")"""
+g.addEdge("A","B")
+g.addEdge("A","J")
+g.addEdge("A","F")
+g.addEdge("B","C")
+g.addEdge("B","J")
+g.addEdge("C","I")
+g.addEdge("C","D")
+g.addEdge("D","H")
+g.addEdge("D","E")
+g.addEdge("E","F")
+g.addEdge("E","G")
+g.addEdge("F","G")
+g.addEdge("G","H")
+g.addEdge("H","I")
+g.addEdge("I","J")
+
 g.getGraph()
+print(g.checkDegrees())
+paths = g.find_all_H_paths("A", "J")
+paths[0] = ["J", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
+#print(g._exchange(paths[0], []))
+g.runThomason(paths[0])
